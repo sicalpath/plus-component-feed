5df821f4937285e93db0ffed789f5a4824007f00
boxshadow <idafoo@sina.com>
2017-02-13 18:14:30 +0800

    create feed

--- src/Controllers/FeedController.php
+++ src/Controllers/FeedController.php
@@ -25,6 +25,21 @@ class FeedController extends Controller
 	            'message' => '动态内容不能为空'
 	        ])->setStatusCode(400);
     	}
+        if(!$request->user_id) {
+            return response()->json([
+                'status' => false,
+                'code' => 6002,
+                'message' => '动态user_id不能为空'
+            ])->setStatusCode(400);
+        }
+        $feed = [];
+        $request->title && $feed['feed_title'] = $request->title;
+        $feed['feed_content'] = $request->content;
+        $feed['feed_client_id'] = $request->getClientIp();
+        $feed['user_id'] = $request->user_id;
+        // 判断动态来路,默认为来自pc
+        $feed['feed_from'] = $request->feed_from ?? 1;
+        $feed['latitude'] = $rqeuest->
     	dump($request->all());
     }
 }
