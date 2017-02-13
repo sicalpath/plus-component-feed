6e9b016f0d58bd1b9190425fc3625ee27c5126c5
白石 <414606094@qq.com>
2017-02-13 15:36:06 +0800

    增加部分接口路由

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ Route::group([
 	Route::get('/feeds/{feed_id}', 'xxx@xxx');
 	//发送分享
 	Route::post('/feeds', 'xxx@xxx');
-	
+	//删除分享
+	Route::delete('/feeds/{feed_id}', 'xxx@xxx');
+	//评论列表
+	Route::get('/feeds/{feed_id}/comments', 'xxx@xxx');
+	//发送评论
+	Route::post('/feeds/{feed_id}/comments', 'xxx@xxx');
+	//删除评论
+	Route::delete('/feeds/{feed_id}/comments/{comment_id}', 'xxx@xxx');
+	//点赞
+	Route::post('/feeds/{feed_id}/diggs', 'xxx@xxx');
+	//取消点赞
+	Route::delete('/feeds/{feed_id}/diggs', 'xxx@xxx');
 });
 
