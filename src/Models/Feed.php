1ddc4a219e552684331f9e8ccb63fba5a343a8ad
boxshadow <idafoo@sina.com>
2017-02-13 14:46:53 +0800

    fixed Feed and FeedComment model

--- src/Models/Feed.php
+++ src/Models/Feed.php
@@ -7,4 +7,76 @@ use Illuminate\Database\Eloquent\Model;
 class Feed extends Model
 {
     protected $table = 'feeds';
+
+    protected $fillable = [
+    	'feed_title',
+    	'feed_content',
+    	'feed_from',
+    	'feed_latitude',
+    	'feed_longtitude',
+    	'feed_client_id',
+    	'feed_goehash'
+    ];
+
+    protected $hidden = [
+    	'feed_client_id'
+    ];
+
+    /**
+     * 单条动态属于一个用户
+     * @return [type] [description]
+     */
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+
+    /**
+     * 动态拥有多条点赞记录
+     * @return [type] [description]
+     */
+    public function diggs()
+    {
+    	return $this->hasMany(FeedDigg::class, 'feed_id');
+    }
+
+
+    /**
+     *动态拥有多条评论
+     * @return [type] [description]
+     */
+    public function comments()
+    {
+    	return $this->hasMany(FeedComment::class, 'feed_id');
+    }
+
+    /**
+     * 一条动态可能会@多个用户
+     * @return [type] [description]
+     */
+    pubilc function atmes()
+    {
+    	return $this->hasMany(FeedAtme::class, 'feed_id');
+    }
+
+    /**
+     * 动态会有多附件信息
+     * @return [type] [description]
+     */
+    public function storages()
+    {
+    	return $this->hasMany(FeedStorage::class, 'feed_id');
+    }
+
+    /**
+     * [scopeByUserId 通过用户ID查找相关动态]
+     * @param  Builder $query [description]
+     * @param  string  $phone [description]
+     * @return [type]         [description]
+     */
+    public function scopeByUserId(Builder $query, integer $userId): Builder
+    {
+        return $query->where('user_id', $userId);
+    }
 }
+
--- src/Models/FeedComment.php
+++ src/Models/FeedComment.php
@@ -7,4 +7,41 @@ use Illuminate\Database\Eloquent\Model;
 class FeedComment extends Model
 {
     protected $table = 'feed_comments';
+
+    protected $fillable = [
+    	'user_id',
+    	'reply_to_user_id',
+    	'feed_id',
+    	'comment_content'
+    ];
+
+    /**
+     * 单条评论属于一个用户
+     * @return [type] [description]
+     */
+    public function user()
+    {
+    	return $this->belongsTo(User::class, 'id', 'user_id');
+    }
+
+    /**
+     * 单条评论属于一条动态
+     * @return [type] [description]
+     */
+    public function feed()
+    {
+    	return $this->belongsTo(Feed::class, 'feed_id', 'feed_id');
+    }
+
+    /**
+     * 根据用户ID查找评论
+     * @param  Builder $query  [description]
+     * @param  integer $userId [description]
+     * @return [type]          [description]
+     */
+    public function scopeByUserId(Builder $query, integer $userId)
+    {
+    	return $query->where('user_id', $userId);
+    }
+
 }
