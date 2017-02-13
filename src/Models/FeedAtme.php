0704fbfd424f0b903a20df81a5b365e8ea765d30
白石 <414606094@qq.com>
2017-02-13 11:45:18 +0800

    atme 和 digg 模型关联

--- src/Models/FeedAtme.php
+++ src/Models/FeedAtme.php
@@ -7,4 +7,37 @@ use Illuminate\Database\Eloquent\Model;
 class FeedAtme extends Model
 {
     protected $table = 'feed_atmes';
+
+    /**
+     * 获取被@用户的基本信息
+     * 
+     * @author bs<414606094@qq.com>
+     * @return object
+     */
+    public function atuser()
+    {
+        return $this->hasOne(User::class, 'at_user_id', 'id');
+    }
+
+    /**
+     * 获取主动@用户的基本信息
+     * 
+     * @author bs<414606094@qq.com>
+     * @return object
+     */
+    public function user()
+    {
+    	return $this->hasOne(User::class, 'user_id', 'id');
+    }
+
+    /**
+     * 获取@的分享内容
+     * 
+     * @author bs<414606094@qq.com>
+     * @return object
+     */
+    public function feed()
+    {
+    	return $this->belongsTo(Feed::class, 'feed_id', 'feed_id'); 
+    }
 }
--- src/Models/FeedDigg.php
+++ src/Models/FeedDigg.php
@@ -7,4 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 class FeedDigg extends Model
 {
     protected $table = 'feed_diggs';
+
+    /**
+     * 赞分享的用户信息
+     * 
+     * @author bs<414606094@qq.com>
+     * @return object
+     */
+    public function user()
+    {
+    	return $this->hasOne(User::class, 'user_id', 'id');
+    }
+
+    /**
+     * 被赞的分享信息
+     * 
+     * @author bs<414606094@qq.com>
+     * @return object
+     */
+    public function feed()
+    {
+    	return $this->belongsTo(Feed::class, 'feed_id', 'feed_id');
+    }
 }
