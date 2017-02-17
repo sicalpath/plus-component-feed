63cfa67f1749b03229e6141d0073e9aafa019a83
boxshadow <idafoo@sina.com>
2017-02-17 17:59:29 +0800

    update migrage for feed_storages

--- database/table_feed_storages_columns.php
+++ database/table_feed_storages_columns.php
@@ -5,12 +5,6 @@ use Illuminate\Support\Facades\Schema;
 
 $component_table_name = 'feed_storages';
 
-if (!Schema::hasColumn($component_table_name, 'user_id')) {
-    Schema::table($component_table_name, function (Blueprint $table) {
-        $table->integer('user_id')->index()->comment('作者UID');
-    });
-}
-
 if (!Schema::hasColumn($component_table_name, 'feed_id')) {
     Schema::table($component_table_name, function (Blueprint $table) {
         $table->integer('feed_id')->index()->unsigned()->comment('动态ID');
