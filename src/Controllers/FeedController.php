d219ba516f0a0cec1b0847e3e19c523471aadcaf
白石 <414606094@qq.com>
2017-03-07 16:43:56 +0800

    动态列表返回多一条评论 用来客户端判断是否可以查看更多

--- src/Controllers/FeedController.php
+++ src/Controllers/FeedController.php
@@ -45,7 +45,7 @@ class FeedController extends Controller
             // 最新3条评论
             $data['comments'] = [];
 
-            $getCommendsNumber = 3;
+            $getCommendsNumber = 4;
             $data['comments'] = $feed->comments()
                 ->orderBy('id', 'desc')
                 ->take($getCommendsNumber)
