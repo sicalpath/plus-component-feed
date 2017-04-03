7db956dea24b9cd0bd8e01ae9b3841440a311771
Seven <shiweidu@outlook.com>
2017-04-03 16:41:56 +0800

    增加渲染逻辑～

--- resource/main.js
+++ resource/main.js
@@ -3,3 +3,15 @@
  *
  * @author Seven Du <shiweidu@outlook.com>
  */
+
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (<div>haha</div>);
+
+document.addEventListener('DOMContentLoaded', () => {
+  render(
+    <App />,
+    document.getElementById('app')
+  );
+});
