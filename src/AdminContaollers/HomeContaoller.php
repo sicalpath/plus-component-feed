186b8d94da3648502e86b840d6ad6f4b96010f3c
Seven <shiweidu@outlook.com>
2017-04-03 16:53:11 +0800

    增加分享管理入口控制器

---
+++ src/AdminContaollers/HomeContaoller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\AdminContaollers;
+
+use Zhiyi\Plus\Http\Controllers\Contaoller;
+use function Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\{
+    base_path
+};
+
+class HomeContaoller extends Contaoller
+{
+    /**
+     * 分享管理后台入口.
+     *
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function show()
+    {
+        return view(base_path('views/admin.blabe.php'));
+    }
+}
