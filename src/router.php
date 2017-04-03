286cb62c1a571e818a3e283321938d42faddab8d
Seven <shiweidu@outlook.com>
2017-04-03 16:46:18 +0800

    增加后台路由

--- src/router.php
+++ src/router.php
@@ -5,6 +5,12 @@ use function Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\base_path as component_
 Route::middleware('web')
     ->namespace('Zhiyi\\Component\\ZhiyiPlus\\PlusComponentFeed\\Controllers')
     ->group(component_base_path('/routes/web.php'));
-Route::prefix('api/v1')
+Route::prefix('/api/v1')
     ->namespace('Zhiyi\\Component\\ZhiyiPlus\\PlusComponentFeed\\Controllers')
     ->group(component_base_path('/routes/api.php'));
+
+// Admin manage routes.
+Route::namespace('Zhiyi\\Component\\ZhiyiPlus\\PlusComponentFeed\\AdminContaollers')
+    ->prefix('/feed/admin')
+    ->middleware('auth:web')
+    ->group(component_base_path('/routes/admin.php'));
