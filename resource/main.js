7e0391f11f8e2b50473eac4bdea84e7b02909d46
Seven <shiweidu@outlook.com>
2017-04-04 15:22:26 +0800

    修改默认入口动画

--- resource/main.js
+++ resource/main.js
@@ -6,10 +6,23 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import theme from './theme';
 
-const App = () => (<div>haha</div>);
+// The app entry.
+const App = () => (
+  <MuiThemeProvider muiTheme={theme}>
+    <div>222</div>
+  </MuiThemeProvider>
+);
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  // Needed for onTouchTap
+  // http://stackoverflow.com/a/34015469/988941
+  injectTapEventPlugin();
+
   render(
     <App />,
     document.getElementById('app')
