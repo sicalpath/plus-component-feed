d7d08f00be8a71b23a0e5136f5136459085280a2
Seven <shiweidu@outlook.com>
2017-04-04 17:58:39 +0800

    完成分享统计信息接口

--- routes/admin.php
+++ routes/admin.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
     Route::get('/', 'HomeContaoller@show')->name('feed:admin');
+    Route::get('/statistics', 'HomeContaoller@statistics');
 });
--- src/AdminContaollers/HomeContaoller.php
+++ src/AdminContaollers/HomeContaoller.php
@@ -2,7 +2,11 @@
 
 namespace Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\AdminContaollers;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Zhiyi\Plus\Http\Controllers\Controller;
+use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\Feed;
+use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\FeedComment;
 use function Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\view;
 
 class HomeContaoller extends Controller
@@ -19,5 +23,29 @@ class HomeContaoller extends Controller
             'base_url' => route('feed:admin'),
             'csrf_token' => csrf_token(),
         ]);
+    }
+
+    /**
+     * 获取分享统计信息.
+     *
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function statistics()
+    {
+        $expiresAt = Carbon::now()->addDay(1);
+
+        $feedsCount = Cache::remember('feeds-count', $expiresAt, function () {
+            return Feed::count();
+        });
+
+        $commentsCount = Cache::remember('feeds-comments-count', $expiresAt, function () {
+            return FeedComment::count();
+        });
+
+        return response()->json([
+            'feedsCount' => $feedsCount,
+            'commentsCount' => $commentsCount,
+        ])->setStatusCode(200);
     }
 }
