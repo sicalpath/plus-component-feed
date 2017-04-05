89e167cf4d897110162341224a198f5fb8f2d398
Seven <shiweidu@outlook.com>
2017-04-05 11:49:01 +0800

    完成分享列表基础UI

--- resource/components/Feeds.js
+++ resource/components/Feeds.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import FeedList from './feeds/List';
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
+
+const tableState = {
+  showCheckboxes: false,
+  showRowHover: true,
+  stripedRows: false,
+};
 
 class FeedsComponent extends Component {
+
+  state = {
+    feeds: [],
+  };
+
   render() {
     return (
       <div>
-        <Route exact component={FeedList}  />
+        <Table>
+          <TableHeader
+            adjustForCheckbox={tableState.showCheckboxes}
+            displaySelectAll={tableState.showCheckboxes}
+          >
+            <TableRow>
+              <TableHeaderColumn>ID</TableHeaderColumn>
+              <TableHeaderColumn>内容</TableHeaderColumn>
+              <TableHeaderColumn>作者</TableHeaderColumn>
+              <TableHeaderColumn>统计</TableHeaderColumn>
+              <TableHeaderColumn>审核</TableHeaderColumn>
+              <TableHeaderColumn>操作</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody
+            displayRowCheckbox={tableState.showCheckboxes}
+            showRowHover={tableState.showRowHover}
+            stripedRows={tableState.stripedRows}
+          >
+            <TableRow>
+              <TableRowColumn>1</TableRowColumn>
+              <TableRowColumn>这是内容</TableRowColumn>
+              <TableRowColumn>Seven</TableRowColumn>
+              <TableRowColumn>100</TableRowColumn>
+              <TableRowColumn>已审核</TableRowColumn>
+              <TableRowColumn></TableRowColumn>
+            </TableRow>
+          </TableBody>
+        </Table>
       </div>
     );
   }
