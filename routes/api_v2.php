3f5f4ebff087ab33376334862ce10827c364c6c5
白石 <414606094@qq.com>
2017-05-25 14:25:27 +0800

    增加部分动态列表接口   更新路由

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -1,3 +1,17 @@
 <?php
+// 最新分享列表
+Route::get('/feeds', 'FeedController@getNewFeeds');
 
-Route::get('/', 'FeedController@getNewFeeds');
+// 热门分享
+Route::get('/feeds/hots', 'FeedController@getHotFeeds');
+
+// 某个用户的分享列表
+Route::get('/user/{user}/feeds', 'FeedController@getUserFeeds')->where(['user' => '[0-9]+']);
+
+Route::middleware('auth:api')
+	->prefix('/feeds')
+	->group(function () {
+		// 关注分享列表
+		Route::get('/follows', 'FeedController@getFollowFeeds');
+
+	});
--- src/Controllers/V2/FeedController.php
+++ src/Controllers/V2/FeedController.php
@@ -2,10 +2,14 @@
 
 namespace Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Controllers\V2;
 
+use DB;
+use Carbon\Carbon;
+use Zhiyi\Plus\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Zhiyi\Plus\Http\Controllers\Controller;
 use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\Feed;
+use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\FeedDigg;
 
 class FeedController extends Controller
 {
@@ -40,6 +44,119 @@ class FeedController extends Controller
         ->get();
 
         return $this->formatFeedList($feeds, $user_id);
+    }
+
+    /**
+     * 获取关注动态列表.
+     *
+     * @author bs<414606094@qq.com>
+     * @param  Request $request [description]
+     */
+    public function getFollowFeeds(Request $request)
+    {
+        $user_id = Auth::guard('api')->user()->id;
+        // 设置单页数量
+        $limit = $request->input('limit') ?? 15;
+        $feeds = Feed::orderBy('id', 'DESC')
+            ->whereIn('user_id', array_merge([$user_id], $request->user()->follows->pluck('following_user_id')->toArray()))
+            ->where(function ($query) use ($request) {
+                if ($request->max_id > 0) {
+                    $query->where('id', '<', $request->max_id);
+                }
+            })
+            ->byAudit()
+            ->with(['storages', 'comments' => function ($query) {
+                $query->orderBy('id', 'desc')
+                    ->take(5)
+                    ->select(['id', 'user_id', 'created_at', 'comment_content', 'reply_to_user_id', 'comment_mark'])
+                    ->get();
+            }, 'diggs' => function ($query) use ($user_id) {
+                $query->where('user_id', $user_id)->first();
+            }, 'collection' => function ($query) use ($user_id) {
+                $query->where('user_id', $user_id)->first();
+            }])
+            ->take($limit)
+            ->get();
+
+        return $this->formatFeedList($feeds, $user_id);
+    }
+
+    /**
+     * 热门动态列表构建.
+     *
+     * @author bs<414606094@qq.com>
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function getHotFeeds(Request $request)
+    {
+        $user_id = Auth::guard('api')->user()->id ?? 0;
+        // 设置单页数量
+        $limit = $request->limit ?? 15;
+        $page = $request->page ?? 1;
+        $skip = ($page - 1) * $limit;
+
+        $feeds = Feed::orderBy('id', 'DESC')
+            ->whereIn('id', FeedDigg::groupBy('feed_id')
+                ->take($limit)
+                ->select('feed_id', DB::raw('COUNT(user_id) as diggcount'))
+                ->where('created_at', '>', Carbon::now()->subMonth()->toDateTimeString())
+                ->orderBy('diggcount', 'desc')
+                ->orderBy('feed_id', 'desc')
+                ->skip($skip)
+                ->pluck('feed_id')
+                )
+            ->byAudit()
+            ->with(['storages', 'comments' => function ($query) {
+                $query->orderBy('id', 'desc')
+                    ->take(5)
+                    ->select(['id', 'user_id', 'created_at', 'comment_content', 'reply_to_user_id', 'comment_mark'])
+                    ->get();
+            }, 'diggs' => function ($query) use ($user_id) {
+                $query->where('user_id', $user_id)->first();
+            }, 'collection' => function ($query) use ($user_id) {
+                $query->where('user_id', $user_id)->first();
+            }])
+            ->get();
+
+        return $this->formatFeedList($feeds, $user_id);
+    }
+
+    /**
+     * 获取单个用户的动态列表.
+     *
+     * @author bs<414606094@qq.com>
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function getUserFeeds(Request $request, User $user)
+    {
+        $auth_id = Auth::guard('api')->user()->id ?? 0;
+        $limit = $request->input('limit', 15);
+        $max_id = intval($request->input('max_id'));
+
+        $feeds = Feed::orderBy('id', 'DESC')
+            ->where('user_id', $user->id)
+            ->where(function ($query) use ($max_id) {
+                if ($max_id > 0) {
+                    $query->where('id', '<', $max_id);
+                }
+            })
+            ->with(['storages', 'comments' => function ($query) {
+                $query->orderBy('id', 'desc')
+                    ->take(5)
+                    ->select(['id', 'user_id', 'created_at', 'comment_content', 'reply_to_user_id', 'comment_mark'])
+                    ->get();
+            }, 'diggs' => function ($query) use ($auth_id) {
+                $query->where('user_id', $auth_id)->first();
+            }, 'collection' => function ($query) use ($auth_id) {
+                $query->where('user_id', $auth_id)->first();
+            }])
+        ->byAudit()
+        ->take($limit)
+        ->get();
+
+        return $this->formatFeedList($feeds, $auth_id);
     }
 
     protected function formatFeedList($feeds, $uid)
