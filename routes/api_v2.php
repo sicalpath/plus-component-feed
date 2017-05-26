af64d56b64ae39a4f1f6f4d430410465510b4558
白石 <414606094@qq.com>
2017-05-26 11:06:54 +0800

    动态接口更新

--- routes/api_v2.php
+++ routes/api_v2.php
@@ -1,5 +1,4 @@
 <?php
-
 // 最新分享列表
 Route::get('/feeds', 'FeedController@getNewFeeds');
 
@@ -9,11 +8,20 @@ Route::get('/feeds/hots', 'FeedController@getHotFeeds');
 // 某个用户的分享列表
 Route::get('/user/{user}/feeds', 'FeedController@getUserFeeds')->where(['user' => '[0-9]+']);
 
+// 获取单条动态
+Route::get('/feed/{feed}', 'FeedController@getSingle');
+
 Route::middleware('auth:api')
-	->prefix('/feeds')
 	->group(function () {
 		// 关注分享列表
-		Route::get('/follows', 'FeedController@getFollowFeeds');
+		Route::get('/feeds/follows', 'FeedController@getFollowFeeds');
 		// 登录用户收藏列表
-		Route::get('/collections', 'FeedController@getUserCollection');
+		Route::get('/feeds/collections', 'FeedController@getUserCollection');
+	    // 增加分享浏览量
+    	Route::patch('/feed/{feed}/viewcount', 'FeedController@addFeedViewCount');
+    	// 发送分享
+    	Route::post('/feed', 'FeedController@store');
+    	// 删除分享
+    	Route::delete('/feed/{feed}', 'FeedController@delete');
+
 	});
