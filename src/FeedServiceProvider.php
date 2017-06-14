1f79732915387a2b41fa41ef846896ecb286c8fc
Seven <shiweidu@outlook.com>
2017-06-14 16:04:51 +0800

    重新设计服务提供者

---
+++ src/FeedServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Zhiyi\Component\ZhiyiPlus\PlusComponentFeed;
+
+use Illuminate\Support\ServiceProvider;
+use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\RouteRegistrar;
+
+class FeedServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the provider.
+     *
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function boot()
+    {
+        $this->routeMap();
+    }
+
+    /**
+     * Register route.
+     *
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    protected function routeMap()
+    {
+        $this->app->make(RouteRegistrar::class)->all();
+    }
+}
