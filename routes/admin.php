f811ffe143513018bcb217d23baff587f71ab7cc
Seven <shiweidu@outlook.com>
2017-06-14 16:05:05 +0800

    重命名路由名称～

--- routes/admin.php
+++ routes/admin.php
@@ -3,12 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@show')->name('feed:admin');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/statistics', 'HomeController@statistics');
-    Route::get('/feeds', 'FeedController@showFeeds');
-    Route::delete('/feeds/{feed}', 'FeedController@deleteFeed');
-    Route::patch('/feeds/{feed}/review', 'FeedController@reviewFeed');
-    Route::get('/comments', 'CommentController@show');
-    Route::delete('/comments/{comment}', 'CommentController@delete');
-});
+Route::get('/statistics', 'HomeController@statistics');
+Route::get('/feeds', 'FeedController@showFeeds');
+Route::delete('/feeds/{feed}', 'FeedController@deleteFeed');
+Route::patch('/feeds/{feed}/review', 'FeedController@reviewFeed');
+Route::get('/comments', 'CommentController@show');
+Route::delete('/comments/{comment}', 'CommentController@delete');
