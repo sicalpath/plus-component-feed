442a27117580dc2a83cf29eca2ff4d748b355a6e
Seven <shiweidu@outlook.com>
2017-06-14 18:29:00 +0800

    增加储存id处理～

--- src/API2/FeedController.php
+++ src/API2/FeedController.php
@@ -2,15 +2,38 @@
 
 namespace Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\API2;
 
+use Illuminate\Http\Request;
 use Zhiyi\Plus\Http\Controllers\Controller;
+use Zhiyi\Plus\Models\StorageTask as StorageTaskModel;
+use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\Feed as FeedModel;
 use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\FormRequest\API2\StoreFeedPost as StoreFeedPostRequest;
 
 class FeedController extends Controller
 {
     public function store(StoreFeedPostRequest $request)
     {
-        dd(
-            $request->all()
-        );
+        $this->fillFeedBaseData($request, $feed = new FeedModel());
+        $this->fillFeedStorages($request, $feed);
+
+        // dd($feed);
+    }
+
+    protected function fillFeedStorages(Request $request, $feed)
+    {
+        
+        $demo = StorageTaskModel::with(['storage'])->whereIn('id', collect($request->input('storage_task'))->mapWithKeys(function ($item, $key) {
+            return [$key => $item['id']];
+        })->filter()->values())->get();
+
+        dd($demo->toArray());
+    }
+
+    protected function fillFeedBaseData(Request $request, FeedModel $feed): FeedModel
+    {
+        foreach ($request->only(['feed_title', 'feed_content', 'feed_from', 'feed_mark', 'latitude', 'longtitude', 'geohash']) as $key => $value) {
+            $feed->$key = $value;
+        }
+
+        return $feed;
     }
 }
