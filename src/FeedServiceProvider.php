0146639531e570e307258bd197de5fb99822d8c9
Seven <shiweidu@outlook.com>
2017-06-14 16:31:56 +0800

    注入处理器

--- src/FeedServiceProvider.php
+++ src/FeedServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Zhiyi\Component\ZhiyiPlus\PlusComponentFeed;
 
+use Zhiyi\Plus\Support\PackageHandler;
 use Illuminate\Support\ServiceProvider;
 
 class FeedServiceProvider extends ServiceProvider
@@ -15,6 +16,7 @@ class FeedServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->routeMap();
+        $this->publishHandler();
 
         // Load views.
         $this->loadViewsFrom(dirname(__DIR__).'/views/', 'feed:view');
@@ -22,6 +24,17 @@ class FeedServiceProvider extends ServiceProvider
         $this->publishes([
             dirname(__DIR__).'/assets' => $this->app->PublicPath().'/zhiyicx/plus-component-feed',
         ]);
+    }
+
+    /**
+     * Publish handler.
+     *
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    protected function publishHandler()
+    {
+        PackageHandler::loadHandleFrom('feed', FeedPackageHandler::class);
     }
 
     /**
