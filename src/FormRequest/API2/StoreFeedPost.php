cc565311803ba38aaf89dc9fc0aecf6831c2af8d
Seven <shiweidu@outlook.com>
2017-06-15 12:13:47 +0800

    更完善的字段判断&权限判断～

--- src/FormRequest/API2/StoreFeedPost.php
+++ src/FormRequest/API2/StoreFeedPost.php
@@ -3,6 +3,7 @@
 namespace Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\FormRequest\API2;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Auth\Access\AuthorizationException;
 
 class StoreFeedPost extends FormRequest
 {
@@ -14,7 +15,8 @@ class StoreFeedPost extends FormRequest
      */
     public function authorize(): bool
     {
-        return $this->user() ? true : false;
+        // 检查认证用户所在用户组是否有发送分享权限.
+        return $this->user()->can('feed-post');
     }
 
     /**
@@ -31,10 +33,12 @@ class StoreFeedPost extends FormRequest
             'feed_from' => 'required|numeric|in:1,2,3,4,5',
             'feed_mark' => 'required|unique:feeds,feed_mark',
             'storage_task' => ['required_without:feed_content', 'array'],
-            'storage_task.*.id' => ['required_with:storage_task', 'exists:storage_tasks,id'],
+            'storage_task.*.id' => ['required_with:storage_task', 'distinct', 'exists:storage_tasks,id'],
+            'storage_task.*.amount' => ['nullable', 'integer'],
             'latitude' => 'required_with:longtitude,geohash',
             'longtitude' => 'required_with:latitude,geohash',
             'geohash' => 'required_with:latitude,longtitude',
+            'amount' => 'nullable|integer',
         ];
     }
 
@@ -54,10 +58,25 @@ class StoreFeedPost extends FormRequest
             'feed_mark.unique' => '请求的内容已存在',
             'storage_task.required_without' => '没有发送任何内容',
             'storage_task.*.id.required_with' => '储存任务ID必须存在',
+            'storage_task.*.id.distinct' => '请求的ID包含重复的值',
             'storage_task.*.id.exists' => '储存任务不存在',
+            'storage_task.*.amount.integer' => '附件收费参数错误',
             'latitude.required_with' => '位置标记不完整',
             'longtitude.required_with' => '位置标记不完整',
             'geohash.required_with' => '位置标记不完整',
+            'amount.integer' => '动态收费参数错误',
         ];
+    }
+
+    /**
+     * Handle a failed authorization attempt.
+     *
+     * @return void
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    protected function failedAuthorization()
+    {
+        throw new AuthorizationException('你没有发布动态权限');
     }
 }
