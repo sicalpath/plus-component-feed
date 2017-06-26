87cd814fa07e01d2f9d06f331e5b1c8f54859433
Seven <shiweidu@outlook.com>
2017-06-26 17:32:16 +0800

    增加固定申请表。

---
+++ database/migrations/2017_06_26_092249_create_pinned_feeds_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePinnedFeedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pinned_feeds', function (Blueprint $table) {
+            $table->increments('id')->comment('固定申请');
+            $table->integer('feed_id')->unsigned()->comment('动态ID');
+            $table->integer('day')->comment('固定期限，单位 天');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pinned_feeds');
+    }
+}
