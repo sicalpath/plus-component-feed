73af08b12af7a07e903a3b6140e28fd45db603ad
Seven <shiweidu@outlook.com>
2017-06-26 17:51:15 +0800

    增加评论置顶申请表。

---
+++ database/migrations/2017_06_26_095028_create_pinned_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePinnedCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pinned_comments', function (Blueprint $table) {
+            $table->increments('id');
+             $table->integer('comment_id')->unsigned()->comment('评论ID');
+            $table->integer('day')->comment('固定期限，单位 天');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pinned_comments');
+    }
+}
