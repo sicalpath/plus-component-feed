7d3b0a4c2d170ef1f72122273903f955896aa66e
Seven <shiweidu@outlook.com>
2017-06-26 11:58:39 +0800

    开发完成评论收费接口。

--- routes/api2.php
+++ routes/api2.php
@@ -7,4 +7,5 @@ Route::get('/feeds', 'FeedController@index');
 Route::get('/feeds/{feed}', 'FeedController@show');
 Route::middleware('auth:api')->group(function () {
     Route::post('/feeds', 'FeedController@store');
+    Route::patch('/feeds/{feed}/comment-paid', 'FeedPayController@commentPaid');
 });
