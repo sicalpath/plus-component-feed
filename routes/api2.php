dd08da4dffdc319f1eac151f28ddc81a41d5e63f
Seven <shiweidu@outlook.com>
2017-06-27 14:07:12 +0800

    增加动态评论路由。

--- routes/api2.php
+++ routes/api2.php
@@ -5,7 +5,13 @@ use Illuminate\Support\Facades\Route;
 // 动态
 Route::get('/feeds', 'FeedController@index');
 Route::get('/feeds/{feed}', 'FeedController@show');
-Route::middleware('auth:api')->group(function () {
-    Route::post('/feeds', 'FeedController@store');
-    Route::patch('/feeds/{feed}/comment-paid', 'FeedPayController@commentPaid');
+Route::middleware('auth:api')->prefix('/feeds')->group(function () {
+    Route::post('/', 'FeedController@store');
+    Route::patch('/{feed}/comment-paid', 'FeedPayController@commentPaid');
 });
+
+// 获取评论
+Route::get('/feeds/{feed}/comments', 'FeedCommentController@index');
+Route::post('/feeds/{feed}/comments', 'FeedCommentController@store');
+Route::get('/feeds/{feed}/comments/{comment}', 'FeedCommentController@show');
+Route::delete('/feeds/{feed}/comments/{comment}', 'FeedCommentController@destroy');
---
+++ src/API2/FeedCommentController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\API2;
+
+use Illuminate\Http\Request;
+use Zhiyi\Plus\Http\Controllers\Controller;
+
+class FeedCommentController extends Controller
+{
+    public function index()
+    {
+        // todo.
+    }
+
+    public function show()
+    {
+        // TODO.
+    }
+
+    public function store()
+    {
+        // TODO.
+    }
+
+    public function destroy()
+    {
+        // TODO
+    }
+}
