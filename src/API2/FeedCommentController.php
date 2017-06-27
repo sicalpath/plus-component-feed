804c2bd5edeeff0c3c0d716720c77975fe6db8b7
Seven <shiweidu@outlook.com>
2017-06-27 15:57:30 +0800

    完成发送动态评论接口。

--- src/API2/FeedCommentController.php
+++ src/API2/FeedCommentController.php
@@ -5,9 +5,12 @@ namespace Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\API2;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
 use Zhiyi\Plus\Http\Controllers\Controller;
+use Zhiyi\Plus\Models\Comment as CommentModel;
 use Illuminate\Contracts\Routing\ResponseFactory as ResponseContract;
 use Illuminate\Contracts\Foundation\Application as ApplicationContract;
 use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\Feed as FeedModel;
+use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\FeedComment as FeedCommentModel;
+use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\FormRequest\API2\StoreFeedComment as CommentFormRequest;
 
 class FeedCommentController extends Controller
 {
@@ -81,9 +84,51 @@ class FeedCommentController extends Controller
         // TODO.
     }
 
-    public function store()
+    /**
+     * Store feed comment.
+     *
+     * @param \Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\FormRequest\API2\StoreFeedComment $request
+     * @param \Illuminate\Contracts\Routing\ResponseFactory $response
+     * @param \Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\Feed $feed
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function store(CommentFormRequest $request, ResponseContract $response, FeedModel $feed)
     {
-        // TODO.
+        $reply_to_user_id = $request->input('reply_to_user_id', 0);
+        $comment_content = $request->input('comment_content');
+        $comment_mark = $request->input('comment_mark');
+        $user = $request->user();
+
+        // 本地评论列
+        $comment = new FeedCommentModel();
+        $comment->user_id = $user->id;
+        $comment->to_user_id = $feed->user_id;
+        $comment->reply_to_user_id = $reply_to_user_id;
+        $comment->comment_content = $comment_content;
+        $comment->comment_mark = $comment_mark;
+        $comment->pinned = 0;
+
+        // 共用评论标记
+        $globalComment = new CommentModel();
+        $globalComment->user_id = $comment->user_id;
+        $globalComment->target_user = $comment->to_user_id;
+        $globalComment->reply_user = $comment->reply_to_user_id;
+        $globalComment->channel = 'feed';
+
+        // 保存本地评论
+        if (! $feed->comments()->save($comment) || ! $comment->id) {
+            return $response->json(['message' => ['评论失败']])->setStatusCode(500);
+        }
+
+        // 保存公用标记
+        $globalComment->target = $comment->id;
+        $globalComment->saveOrFail();
+
+        return $response->json([
+            'message' => '评论成功',
+            'id' => $comment->id,
+        ])->setStatusCode(201);
     }
 
     public function destroy()
