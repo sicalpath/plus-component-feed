d8dd2675ba024291fb0c420e79f25060c213af69
Seven <shiweidu@outlook.com>
2017-06-27 15:09:23 +0800

    完成动态评论列表获取。

--- src/API2/FeedCommentController.php
+++ src/API2/FeedCommentController.php
@@ -2,13 +2,78 @@
 
 namespace Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\API2;
 
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
 use Zhiyi\Plus\Http\Controllers\Controller;
+use Illuminate\Contracts\Routing\ResponseFactory as ResponseContract;
+use Illuminate\Contracts\Foundation\Application as ApplicationContract;
+use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\Feed as FeedModel;
 
 class FeedCommentController extends Controller
 {
-    public function index()
+    /**
+     * The application instance.
+     *
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    protected $app;
+
+    /**
+     * Create the contoller instance.
+     *
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function __construct(ApplicationContract $app)
     {
-        // todo.
+        $this->app = $app;
+        $this->middleware('auth:api')->except(['index']);
+    }
+
+    /**
+     * Feed comments.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Illuminate\Contracts\Routing\ResponseFactory $[name]
+     * @param \Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\Feed $feed
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function index(Request $request, ResponseContract $response, FeedModel $feed)
+    {
+        $limit = $request->query('limit', 20);
+        $after = $request->query('after');
+
+        $comments = $feed->comments()
+            ->when((bool) $after, function (Builder $query) use ($after) {
+                return $query->where('id', '<', $after);
+            })
+            ->where('pinned', 0)
+            ->limit($limit)
+            ->get();
+
+        return $response->json([
+            'comments' => $comments,
+            'pinned' => $this->getPinnedComment($request, $feed),
+        ])->setStatusCode(200);
+    }
+
+    /**
+     * Get feed pinned comments.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\Feed $feed
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function getPinnedComment(Request $request, FeedModel $feed)
+    {
+        if (! $request->query('after')) {
+            return [];
+        }
+
+        return $feed->comments()
+            ->where('pinned', 1)
+            ->get()->all();
     }
 
     public function show()
