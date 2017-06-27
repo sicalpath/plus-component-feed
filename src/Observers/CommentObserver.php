0688815379f48adb007d11c79c80d5f97c0124e1
Seven <shiweidu@outlook.com>
2017-06-27 16:41:11 +0800

    完成评论事件监听。

---
+++ src/Observers/CommentObserver.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Observers;
+
+use Zhiyi\Plus\Models\Comment;
+use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\FeedComment;
+
+class CommentObserver
+{
+    /**
+     * Global Comment deleted.
+     *
+     * @param \Zhiyi\Plus\Models\Comment $comment
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function deleted(Comment $comment)
+    {
+        return $this->validateOr($comment, function (FeedComment $feedComment) {
+            $feedComment->delete();
+        });
+    }
+
+    /**
+     * Fetch event.
+     *
+     * @param \Zhiyi\Plus\Models\Comment $comment
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function fetch(Comment $comment)
+    {
+        return $this->validateOr($comment, function (FeedComment $feedComment) {
+            return new Fetch\CommentFetch($feedComment);
+        });
+    }
+
+    /**
+     * Validate or run call.
+     *
+     * @param \Zhiyi\Plus\Models\Comment $comment
+     * @param callable $call
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    protected function validateOr(Comment $comment, callable $call)
+    {
+        if (! $comment->channel === 'feed' || ! ($comment = FeedComment::find($channel->target))) {
+            return null;
+        }
+
+        return call_user_func_array($call, [$comment]);
+    }
+}
