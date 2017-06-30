4adc9ded19d250e52c651872eb131c83127943c2
Seven <shiweidu@outlook.com>
2017-06-30 14:53:12 +0800

    完成收藏接口。

--- src/API2/FeedCollectionController.php
+++ src/API2/FeedCollectionController.php
@@ -2,13 +2,33 @@
 
 namespace Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\API2;
 
+use Illuminate\Http\Request;
 use Zhiyi\Plus\Http\Controllers\Controller;
+use Illuminate\Contracts\Routing\ResponseFactory as ResponseContract;
+use Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\Feed as FeedModel;
 
 class FeedCollectionController extends Controller
 {
-    public function store()
+    /**
+     * 收藏动态.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Illuminate\Contracts\Routing\ResponseFactory $response
+     * @param \Zhiyi\Component\ZhiyiPlus\PlusComponentFeed\Models\Feed $feed
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function store(Request $request, ResponseContract $response, FeedModel $feed)
     {
-        // todo
+        $user = $request->user()->id;
+
+        if ($feed->collected($user)) {
+            return $response->json(['message' => ['已经收藏过']])->setStatusCode(422);
+        }
+
+        $feed->collect($user);
+
+        return $response->json(['message' => ['收藏成功']])->setStatusCode(201);
     }
 
     public function destroy()
