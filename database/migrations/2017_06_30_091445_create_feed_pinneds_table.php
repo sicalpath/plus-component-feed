7ff9f3449e3e64a1219dd13b773176be877376e5
Seven <shiweidu@outlook.com>
2017-06-30 19:13:36 +0800

    增加目标用户字段。

--- database/migrations/2017_06_30_091445_create_feed_pinneds_table.php
+++ database/migrations/2017_06_30_091445_create_feed_pinneds_table.php
@@ -16,8 +16,9 @@ class CreateFeedPinnedsTable extends Migration
         Schema::create('feed_pinneds', function (Blueprint $table) {
             $table->increments('id');
             $table->string('channel')->comment('频道');
-            $table->integer('target')->unsigned()->comment('评论ID');
+            $table->integer('target')->unsigned()->comment('目标ID');
             $table->integer('user_id')->unsigned()->comment('用户');
+            $table->integer('target_user')->unsigned()->nullable()->default(null)->comment('目标用户');
             $table->integer('amount')->unsigned()->comment('金额');
             $table->integer('day')->comment('固定期限，单位 天');
             $table->timestamp('expires_at')->nullable()->comment('到期时间，固定后设置该时间');
